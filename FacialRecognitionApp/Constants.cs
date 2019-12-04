862572339ae37b0f8dc3d08c5f834bb2ee1be028
Sergio Velmay <sergiovelmay@gmail.com>
2019-12-04 15:38:31 +0100

    Desktop project ready added.

---
+++ FacialRecognitionApp/Constants.cs
@@ -0,0 +1,64 @@
+﻿namespace FacialRecognitionDoor
+{
+    /// <summary>
+    /// General constant variables
+    /// </summary>
+    public static class GeneralConstants
+    {
+        // This variable should be set to false for devices that have GPU support. Set it to true for Raspberry Pi.
+        public const bool DisableLiveCameraFeed = false;
+
+        // Azure Face API Primary Key should be entered here
+        public const string FaceResourceKey = "xxxxxxxxxxxxxxx";
+
+        // Enter the API endpoint address.
+        public const string FaceResourceName = "xxxxxxxxxxxx"; 
+        public const string FaceResourceEndpoint = "https://" + FaceResourceName + ".cognitiveservices.azure.com/face/v1.0";
+
+        // Name of the folder in which all Whitelist data is stored
+        // TODO: Folder name without blank spaces for Raspberry Pi and Windows IoT
+        public const string WhiteListFolderName = "FacialRecognitionDoorWhitelist";
+
+        // Fixed ID for the shared WhiteList
+        public const string FixedWhiteListID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+    }
+
+    /// <summary>
+    /// Constant variables that hold messages to be read via the SpeechHelper class
+    /// </summary>
+    public static class SpeechContants
+    {
+        public const string InitialGreetingMessage = "Welcome to the Idiwork Facial Recognition project!";
+
+        public const string ProjectReadyMessage = "The Idiwork Facial Recognition door is ready now!";
+
+        public const string VisitorNotRecognizedMessage = "Sorry! I don't recognize you, so I cannot open the door.";
+
+        public const string NoCameraMessage = "Sorry! It seems like your camera has not been initialized.";
+
+        public static string AddVisitorsMessage = "Adding whitelisted visitors.";
+
+        public static string GeneralGreetingMessage(string visitorName)
+        {
+            return "Welcome " + visitorName + "! I will open the door for you.";
+        }
+    }
+
+    /// <summary>
+    /// Constant variables that hold values used to interact with device GPIO (Rasberry Pi 2 model B).
+    /// </summary>
+    public static class GpioConstants
+    {
+        // The GPIO pin that the doorbell button is attached to.
+        public const int ButtonPinID = 5;
+
+        // The GPIO pin that the door LED light is attached to.
+        public const int DoorLockPinID = 4;
+
+        // The GPIO pin that the door servo motor is attached to.
+        public const int ServoMotorPinID = 18;
+
+        // The amount of time in seconds that the door will remain unlocked.
+        public const int DoorLockOpenDurationSeconds = 10;
+    }
+}
---
+++ FacialRecognitionApp/Helpers/SpeechHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Media.SpeechSynthesis;
+using Windows.UI.Xaml.Controls;
+
+namespace FacialRecognitionDoor.Helpers
+{
+    /// <summary>
+    /// Utilizes SpeechSynthesizer to convert text to an audio message played through a XAML MediaElement
+    /// </summary>
+    class SpeechHelper : IDisposable
+    {
+        private MediaElement mediaElement;
+        private SpeechSynthesizer synthesizer;
+
+        /// <summary>
+        /// Accepts a MediaElement that should be placed on whichever page user is on when text is read by SpeechHelper.
+        /// Initializes SpeechSynthesizer.
+        /// </summary>
+        public SpeechHelper(MediaElement media)
+        {
+            mediaElement = media;
+            synthesizer = new SpeechSynthesizer();
+            // TODO: Select the correct voice from device's Operation System
+            var list = from a in SpeechSynthesizer.AllVoices
+                       where a.Language.Contains("en-US")
+                       //where a.Language.Contains("es-ES") // Select spanish voice
+                       //where a.Gender == VoiceGender.Female // Select female voice
+                       select a;
+
+            if (list.Count() > 0)
+            {
+                synthesizer.Voice = list.Last();
+            }
+        }
+
+        /// <summary>
+        /// Synthesizes passed through text as audio and plays speech through the MediaElement first sent through.
+        /// </summary>
+        public async Task Read(string text, double seconds)
+        {
+            if (mediaElement != null && synthesizer != null)
+            {
+                var stream = await synthesizer.SynthesizeTextToStreamAsync(text);
+                mediaElement.AutoPlay = true;
+                mediaElement.SetSource(stream, stream.ContentType);
+                mediaElement.Play();
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+        }
+
+        /// <summary>
+        /// Disposes of IDisposable type SpeechSynthesizer
+        /// </summary>
+        public void Dispose()
+        {
+            synthesizer.Dispose();
+        }
+    }
+}
